df37af667bc74d9d39122141a98e96e740ed56fc
Sergei Astapenko <publetter@gmail.com>
2016-05-13 16:38:36 +0300

    Other tasks added

--- StringDigitSum.js
+++ StringDigitSum.js
@@ -2,10 +2,10 @@ var str1 = '123x1z13', str2 = 'a123';
 var reg = /[G-Zg-z]/;
 str1 = str1.split(reg).join("");
 str2 = str2.split(reg).join("");
-var res = sum(str1) + sum(str2);
+var res = sumDigit(str1) + sumDigit(str2);
 console.log(res);
 
-function sum(str){
+function sumDigit(str){
     var sum = 0;
     for(var i = 0; i < str.length; i++) {
         sum += parseInt(str[i], 16);
